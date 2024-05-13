efd9ec46d5396c1444bbc1994c4de8d6285737f6
Santiago Manuel <sanmanmedper2020@gmail.com>
2024-05-13 18:29:01 +0200

    fix: Correcciones en la recogida de datos del formulario y en el envio al servidor

--- js/mod.js
+++ js/mod.js
@@ -1,16 +1,22 @@
-let nombre, categoria, precio, desarrollador, imagen, website;
-
+let nombre, categoria, precio, desarrollador, imagen, website, plataformas, idiomas, genero, clasificacion, version;
+const guardar = document.getElementById('guardar');
 // Alta de datos
-function altaJuego(event) {
+guardar.addEventListener('click', (event) => {
+    console.log('me han clicado')
     event.preventDefault();
 
     // Obtengo el contenido de las variables
     nombre = document.querySelector('input[name="nombre"]').value;
-    categoria = document.querySelector('select[name="Categoria"]').value;
+    categoria = document.querySelector('select[name="categoria"]').value;
+    genero = document.querySelector('input[name="genero"]').value;
     precio = document.querySelector('input[name="precio"]').value;
-    desarrollador = document.querySelector('select[name="Desarrollador"]').value;
+    desarrollador = document.querySelector('select[name="desarrolladores"]').value;
     imagen = document.querySelector('input[type="file"]').files[0];
     website = document.querySelector('input[name="website"]').value;
+    plataformas = document.querySelector('select[name="plataformas"]').value;
+    idiomas = document.querySelector('select[name="idiomas"]').value;
+    clasificacion = document.querySelector('select[name="clasificacion"]').value;
+    version = document.querySelector('input[name="version"]:checked');
 
     // Compruebo que los campos no esten vacios
     if (nombre === '' || categoria === '' || precio === '' || desarrollador === '' || !imagen || website === '') {
@@ -22,7 +28,7 @@ function altaJuego(event) {
     if (imagen.size > 1048576) {
         alert('La imagen es demasiado grande');
         return;
-    }  
+    }
 
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost/RETOLMS/server/InsertGame.php', true);
@@ -31,19 +37,21 @@ function altaJuego(event) {
     let formData = new FormData();
     formData.append('nombre', nombre);
     formData.append('categoria', categoria);
+    formData.append('genero', genero);
     formData.append('precio', precio);
     formData.append('desarrollador', desarrollador);
     formData.append('imagen', imagen);
-    formData.append('website', website);
+    formData.append('web', website);
+    formData.append('plataformas', plataformas);
+    formData.append('idiomas', idiomas);
+    formData.append('clasificacion', clasificacion);
+    formData.append('version', version);
 
     // Enviar los datos del formulario
-    xhr.send(formData);
-
     xhr.onreadystatechange = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 console.log('Datos guardados correctamente');
-                update();
             } else if (xhr.status === 404) {
                 console.log('Página no encontrada');
             } else {
@@ -51,8 +59,5 @@ function altaJuego(event) {
             }
         }
     }
-}
-
-// Obtener el formulario y añadir un manejador de eventos para el envío
-const form = document.querySelector('form');
-form.addEventListener('submit', altaJuego);
+    xhr.send(formData);
+})
